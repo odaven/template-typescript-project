c804a93e01e704c549aa7345322934cafbb9f892
odaven <18171281+odaven@users.noreply.github.com>
2022-10-05 09:26:24 +0100

    Some tweaks

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
 // This is needed because Jest does not understand typescript so we need it to me transformed into js first
-module.exports = {
+import type {Config} from 'jest';
+const config: Config = {
+  verbose: true,
     preset: "ts-jest",
     testEnvironment: "node",
     transform: {
         "^.+\\.ts?$": "ts-jest",
     },
     transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    testMatch: [ "**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)" ]
 };
+
+export default config;
